96f16948a321986d6425a0a7a971942292195cbc
Tatyana_Reshetova <vlasenko2205@gmail.com>
2023-03-17 23:46:39 +0300

    Разница между максимальным и минимальным элементом массива

---
+++ Exercise38_DifferenceMaxMin/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 38: Задайте массив целых чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+//[3 7 22 2 78] -> 76
+
+int [] GetRandomArray (int size, int minValue, int maxValue)
+{
+    int[] result = new int [size];
+    for (int i = 0; i<size;i++)
+    {
+        result [i] = new Random().Next(minValue,maxValue+1);
+    }
+    return result;
+}
+
+int [] Difference (int [] array)
+{
+    int min = array[0];
+    int max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]<min) {min = array[i];}
+    }
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>max) {max = array[i];}
+    }
+    int [] Diff = {max - min};
+    return Diff;
+} 
+
+Console.Clear();
+int [] array = GetRandomArray (4, 1, 100);
+Console.WriteLine($"[   {String.Join(", ", array)}   ] -> {String.Join(", ", Difference(array))} ");
