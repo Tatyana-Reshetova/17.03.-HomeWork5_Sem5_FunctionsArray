be0532f48f18599df9a83d69413fa01fb5a97c64
Tatyana_Reshetova <vlasenko2205@gmail.com>
2023-03-17 23:21:43 +0300

    Сумма элементов массива на нечетных позициях

---
+++ Exercise36_SumUnevenPosition/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+//[3, 7, 23, 12] -> 19
+
+//[-4, -6, 89, 6] -> 0
+
+int [] GetRandomArray (int size, int minValue, int maxValue)
+{
+    int[] result = new int [size];
+    for (int i = 0; i<size;i++)
+    {
+        result [i] = new Random().Next(minValue,maxValue+1);
+    }
+    return result;
+}
+
+int unevenPosition (int [] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i = i+2)
+    {
+        sum=sum+array[i];
+    }
+    return sum;
+} 
+
+Console.Clear();
+int [] array = GetRandomArray (4, 2, 15);
+Console.WriteLine($"[   {String.Join(", ", array)}   ] -> {unevenPosition(array)}");
